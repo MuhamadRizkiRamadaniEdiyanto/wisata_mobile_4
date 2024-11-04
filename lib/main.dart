41ff02f69347bef8f56c409fa72f81bd6dc0fb1e
Muhamad Rizki <muhamadrizki131103@gmail.com>
2024-11-04 12:58:51 +0700

    membuat splasscreen

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:project_travel/latihan_modul/latihanp5.dart';
+import 'package:project_travel/splash_screen.dart';
+// import 'package:project_travel/splash_screen.dart';
+
+void main(){
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
